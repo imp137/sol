28950197c0e830a41355e72d22ff9842cbd06b8e
imp137 <impgun1337@gmail.com>
2024-12-14 01:29:26 +0300

    Update file.sol
    Added first bunch of stuff.

--- file.sol
+++ file.sol
@@ -1,1 +1,16 @@
+// SPDX-License_Identifier: MIT
+pragma solidity 0.8.19;
+
+contract XXX {
+  mapping(address => uint) xxx;
+
+  function setXXX(address _addr, uint val) public {
+    xxx[_addr] = val;
+  }
+
+  function getXXX(address _addr) public view returns(uint) {
+    return xxx[_addr];
+  }
+}
+
 
